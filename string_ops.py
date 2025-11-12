8162dc4e888e73976c26f18d4a4c91f079ea80df
Daniel <d0583258844@gmail.com>
2025-11-12 17:42:18 +0200

    created the main file called string ops, its contain all the function. new i added the first one

--- string_ops.py
+++ string_ops.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/reverse/{text}")
+@app.get("reverse/{text}")
 def reverse_str(text: str):
     return { "original": text, "reversed_text": text[::-1] }
