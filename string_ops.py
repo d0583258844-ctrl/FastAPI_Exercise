5eba31e9000d6f7217b574a2251f9ff8497cf2bc
Daniel <d0583258844@gmail.com>
2025-11-12 18:24:30 +0200

    I added the function uppercase inside the string ops

--- string_ops.py
+++ string_ops.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("reverse/{text}")
+@app.get("/reverse/{text}")
 def reverse_str(text: str):
-    return { "original": text, "reversed_text": text[::-1] }
+    return { "original": text, "reversed_text": text[::-1]}
+
+
+@app.get("/uppercase/{text}")
+def to_upper(text: str):
+    return { "original": text, "uppercased": text.upper()}
