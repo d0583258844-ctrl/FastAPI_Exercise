4a04348da6ed61006887d8c3059861ba474ba7d3
Daniel <d0583258844@gmail.com>
2025-11-12 19:12:02 +0200

    I added the second func remove vowels

--- string_ops.py
+++ string_ops.py
@@ -11,3 +11,13 @@ def reverse_str(text: str):
 @app.get("/uppercase/{text}")
 def to_upper(text: str):
     return { "original": text, "uppercased": text.upper()}
+
+
+@app.post("/remove-vowels")
+def remove_vowels(text: str):
+    letters = ["a", "e", "i", "o", "u"]
+    new_text = ""
+    for ch in text:
+        if ch not in letters:
+            new_text = new_text+ ch
+    return { "original": text, "without_vowels": new_text }
